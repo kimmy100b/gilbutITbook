ad1463a1bff14b399c503db0681c49d9fa40cbc3
DESKTOP-J5N5A82\LG <kimmy100b@gmail.com>
2020-12-30 21:46:00 +0900

    [#1] 4 - 6 장

---
+++ pro06/src/sec02/ex01/LoginServlet2.java
@@ -0,0 +1,40 @@
+package sec02.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/login2")
+public class LoginServlet2 extends HttpServlet {
+	
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("init 메서드 호출");
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		String id = request.getParameter("user_id");
+		String pw = request.getParameter("user_pw");
+		
+		String data = "<html>";
+		data += "<body>";
+		data += "아이디 : " + id;
+		data += "<br>";
+		data += "패스워드 : " + id;
+		data += "</body>";
+		data += "</html>";
+		out.print(data);
+	}
+
+	public void destroy() {
+		System.out.println("destory 메서드 호출");
+	}
+}
