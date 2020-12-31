bd3709c06527b948812026ae97c5bc14e2add043
DESKTOP-J5N5A82\LG <kimmy100b@gmail.com>
2020-12-31 20:48:53 +0900

    [#2] 7.2 서블릿의 데이터베이스 연동하기

---
+++ pro07/src/sec01/ex01/MemberDAO.java
@@ -0,0 +1,62 @@
+package sec01.ex01;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class MemberDAO {
+	private static final String driver = "com.mysql.jdbc.Driver"; 
+	private static final String url = "jdbc:mysql://localhost:3306/bookShop?serverTimezone=Asia/Seoul";
+	private static final String user = "root";
+	private static final String pwd = "mysql";
+
+	private Statement stmt;
+	private Connection con;
+	
+	public List<MemberVO> listMembers() {
+		List<MemberVO> list = new ArrayList<MemberVO>();
+		try {
+			connDB();
+			String query = "select * from t_member ";
+			System.out.println(query);
+			ResultSet rs = stmt.executeQuery(query);
+			while (rs.next()) {
+				String id = rs.getString("id");
+				String pwd = rs.getString("pwd");
+				String name = rs.getString("name");
+				String email = rs.getString("email");
+				Date joinDate = rs.getDate("joinDate");
+				MemberVO vo = new MemberVO();
+				vo.setId(id);
+				vo.setPwd(pwd);
+				vo.setName(name);
+				vo.setEmail(email);
+				vo.setJoinDate(joinDate);
+				list.add(vo);
+			}
+			rs.close();
+			stmt.close();
+			con.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+	private void connDB() {
+		try {
+			Class.forName(driver);
+			System.out.println("MySQL 드라이버 로딩 성공");
+			con = DriverManager.getConnection(url, user, pwd);
+			System.out.println("Connection 생성 성공");
+			stmt = con.createStatement();
+			System.out.println("Statement 생성 성공");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
