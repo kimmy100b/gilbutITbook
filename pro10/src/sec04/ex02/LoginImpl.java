c32405126ccf61bb2b444c268631be9991b8fe7c
DESKTOP-J5N5A82\LG <kimmy100b@gmail.com>
2021-01-02 20:51:03 +0900

    [#8] 10.4 여러 가지 서블릿 관련 Listener API

---
+++ pro10/src/sec04/ex02/LoginImpl.java
@@ -0,0 +1,36 @@
+package sec04.ex02;
+
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+/**
+ * Application Lifecycle Listener implementation class LoginImpl
+ *
+ */
+@WebListener
+public class LoginImpl implements HttpSessionListener {
+	String user_id;
+	String user_pw;
+	static int total_user = 0;
+	
+    public LoginImpl() {
+		
+	}
+
+	public LoginImpl(String user_id, String user_pw) {
+		this.user_id = user_id;
+		this.user_pw = user_pw;
+	}
+	
+    public void sessionCreated(HttpSessionEvent arg0)  { 
+    	System.out.println("技记 积己");
+		++total_user;
+    }
+
+    public void sessionDestroyed(HttpSessionEvent arg0)  { 
+    	System.out.println("技记 家戈");
+		total_user--;
+    }
+	
+}
