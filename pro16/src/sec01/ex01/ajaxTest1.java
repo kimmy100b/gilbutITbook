bf6b313d44450d9db1128fa4a9c159daec3db0f1
DESKTOP-J5N5A82\LG <kimmy100b@gmail.com>
2021-01-07 12:30:30 +0900

    [#19] 16.5.2 제이쿼리 Ajax 사용하기

---
+++ pro16/src/sec01/ex01/ajaxTest1.java
@@ -0,0 +1,31 @@
+package sec01.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/ajaxTest1")
+public class ajaxTest1 extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+	
+	private void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=utf-8");
+		String param = (String)request.getParameter("param");
+		System.out.println("param = "+param);
+		PrintWriter writer = response.getWriter();
+		writer.print("안녕하세요! 서버입니다.");
+	}
+
+}
