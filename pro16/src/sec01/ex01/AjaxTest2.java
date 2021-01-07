56f7ef935c2b7143bb8ce3c59bab5709fff19fa2
DESKTOP-J5N5A82\LG <kimmy100b@gmail.com>
2021-01-07 13:08:56 +0900

    [#19] 16.5.3 XML 데이터 연동하기

--- pro16/src/sec01/ex01/AjaxTest2.java
+++ pro16/src/sec01/ex01/AjaxTest2.java
@@ -9,23 +9,34 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/ajaxTest1")
-public class ajaxTest1 extends HttpServlet {
+@WebServlet("/ajaxTest2")
+public class AjaxTest2 extends HttpServlet {
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doHandle(request, response);
 	}
-
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doHandle(request, response);
 	}
+
 	
 	private void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
 		response.setContentType("text/html; charset=utf-8");
-		String param = (String)request.getParameter("param");
-		System.out.println("param = "+param);
+		String result = "";
 		PrintWriter writer = response.getWriter();
-		writer.print("안녕하세요! 서버입니다.");
+		result = "<main><book>"+
+		         "<title><![CDATA[초보자를 위한 자바 프로그래밍]]></title>" +
+		         "<writer><![CDATA[인포북스 저 | 이병승]]></writer>" +                             
+		         "<image><![CDATA[http://localhost:8080/pro16/image/image1.jpg]]></image>"+
+		      "</book>"+
+		      "<book>"+
+		         "<title><![CDATA[모두의 파이썬]]></title>" +
+		         "<writer><![CDATA[길벗 저 | 이승찬]]></writer>" +                 
+		        "<image><![CDATA[http://localhost:8080/pro16/image/image2.jpg]]></image>"+
+		      "</book></main>";
+		System.out.println(result);
+		writer.print(result);
 	}
-
 }
